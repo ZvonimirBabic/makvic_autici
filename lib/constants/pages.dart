fb63e01a0a50d46224c706343b1c775aa3f61d2d
BlackWW <zvobabic@hotmail.com>
2022-11-20 17:41:38 +0100

    Add: Splash screen

--- lib/constants/pages.dart
+++ lib/constants/pages.dart
@@ -1,6 +1,15 @@
 import 'package:get/get.dart';
 
-const List<GetPage> makvicAuticiPages = <GetPage>[];
+import '../ui/splash/splash_screen_binding.dart';
+import '../ui/splash/splash_screen_view.dart';
+
+List<GetPage> makvicAuticiPages = <GetPage>[
+  GetPage(
+    name: MakvicAuticiRoutes.splashScreen,
+    page: SplashScreenView.new,
+    binding: SplashScreenBinding(),
+  ),
+];
 
 class MakvicAuticiRoutes {
   static const splashScreen = '/splash_screen';
